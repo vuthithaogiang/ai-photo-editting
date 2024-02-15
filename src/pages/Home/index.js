80d971d378497e332dfc6204d5eb5b4c6b695829
giang <vuthithaogiang050400@gmail.com>
2024-02-15 10:12:44 +0700

    add scroll to top

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,8 +6,13 @@ import Reviews from './Reviews';
 import Blogs from '~/components/Blogs';
 import QuestionsAndAnswers from '~/components/QuestionsAndAnswers';
 import Dowload from '~/components/Dowload';
+import { useEffect } from 'react';
 
 function Home() {
+      useEffect(() => {
+            // Scroll the body to x = 0, y = 0
+            window.scrollTo(0, 0); // Change the values (0, 0) to the desired scroll position
+      }, []);
       return (
             <main>
                   <BigSection />
--- src/pages/RemoveBackground/index.js
+++ src/pages/RemoveBackground/index.js
@@ -4,10 +4,15 @@ import QuestionsAndAnswers from '~/components/QuestionsAndAnswers';
 import Feature from './Feature';
 import HowItWorks from './HowItWorks';
 import IntroHero from './IntroHero';
+import { useEffect } from 'react';
 
 const cx = classNames.bind(styles);
 
 function RemoveBackground() {
+      useEffect(() => {
+            // Scroll the body to x = 0, y = 0
+            window.scrollTo(0, 0); // Change the values (0, 0) to the desired scroll position
+      }, []);
       return (
             <div className={cx('wrapper')}>
                   <Feature />
