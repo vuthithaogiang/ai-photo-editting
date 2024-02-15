7b8e7d882a9c3221d13983218e8caf1d5e83f22d
giang <vuthithaogiang050400@gmail.com>
2024-02-15 20:25:46 +0700

    add sign up

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,23 +2,65 @@ import classNames from 'classnames/bind';
 import styles from './SignUp.module.scss';
 import icons from '~/assets/icons';
 import images from '~/assets/images';
+import React, { useState, useRef, useEffect } from 'react';
 
 const cx = classNames.bind(styles);
 
 function SignUp() {
+      const [showPassword, setShowPassword] = useState(false);
+      const [showConfirmationPassword, setShowConfirmationPassword] = useState(false);
+
+      const [email, setEmail] = useState('');
+      const [emailValid, setEmailValid] = useState(false);
+      const [emailFocus, setEmailFocus] = useState(false);
+
+      const [password, setPassword] = useState('');
+      const [passwordValid, setPasswordValid] = useState(false);
+      const [passwordFocus, setPasswordFocus] = useState(false);
+
+      const [confirmPassword, setConfirmPassword] = useState('');
+      const [matchPassword, setMatchPassword] = useState(false);
+      const [confirmPasswordFocus, setConfirmPasswordFocus] = useState(false);
+
+      const [acceptTerm, setAcceptTerms] = useState(false);
+      const emailRef = useRef();
+
+      useEffect(() => {
+            if (!emailRef.current) return;
+
+            emailRef.current.focus();
+      }, []);
+
+      useEffect(() => {}, [email]);
+
+      const toggleShowPassword = () => {
+            setShowPassword(!showPassword);
+      };
+
+      const toggleShowconfirmationPassword = () => {
+            setShowConfirmationPassword(!showConfirmationPassword);
+      };
+
+      const handleSubmitForm = async (e) => {
+            e.preventDefault();
+      };
+
       return (
             <div className={cx('wrapper')}>
                   <div className="container">
                         <div className="auth">
                               <div className="auth__content">
+                                    {/* Logo */}
                                     <div className="auth__content-logo">
                                           <img alt="PixelPro" src={icons.authLogo} />
                                     </div>
 
+                                    {/* Switch Buttons */}
                                     <div className="auth__content-switch-btns">
                                           <button>Sign Up</button>
                                           <button>Sign In</button>
                                     </div>
+                                    {/* APIs */}
                                     <div className="auth__content-api">
                                           <div className="auth__content-api-card">
                                                 <img alt="" src={icons.images} />
@@ -31,6 +73,7 @@ function SignUp() {
                                                 <p className="auth__content-api-text">per month</p>
                                           </div>
                                     </div>
+                                    {/* Socials */}
                                     <div className="auth__content-socials">
                                           <div className="auth__content-social google">
                                                 <img alt="google" src={icons.google} />
@@ -42,53 +85,108 @@ function SignUp() {
                                                 <img alt="facebook" src={icons.facebookWhite} />
                                           </div>
                                     </div>
-
+                                    {/* Separate */}
                                     <div className="auth__content-separate">
                                           <span className="auth__content-separate-line"></span>
                                           <span className="auth__content-separate-text">Or Continue with</span>
                                           <span className="auth__content-separate-line"></span>
                                     </div>
+                                    {/* Form Sign Up */}
                                     <div className="auth__content-form">
-                                          <form className="form">
+                                          <form onSubmit={handleSubmitForm} className="form">
+                                                {/* Email */}
                                                 <div className="form__group">
-                                                      <div className="form__row border active">
+                                                      <div
+                                                            className={
+                                                                  emailFocus
+                                                                        ? 'form__row border active'
+                                                                        : 'form__row border'
+                                                            }
+                                                      >
                                                             <div className="form__control">
                                                                   <input
+                                                                        ref={emailRef}
                                                                         className="form__input"
                                                                         type="email"
                                                                         placeholder="Email"
+                                                                        value={email}
+                                                                        onChange={(e) => setEmail(e.target.value)}
+                                                                        onFocus={() => setEmailFocus(true)}
+                                                                        onBlur={() => setEmailFocus(false)}
                                                                   />
                                                             </div>
                                                       </div>
+
+                                                      {emailFocus && email !== '' && !emailValid && (
+                                                            <div className="form__error">Please enter email valid!</div>
+                                                      )}
                                                 </div>
+
+                                                {/* Password */}
                                                 <div className="form__group">
-                                                      <div className="form__row border">
+                                                      <div
+                                                            className={
+                                                                  passwordFocus
+                                                                        ? 'form__row border active'
+                                                                        : 'form__row border'
+                                                            }
+                                                      >
                                                             <div className="form__control">
                                                                   <input
-                                                                        type="password"
+                                                                        type={showPassword ? 'text' : 'password'}
                                                                         placeholder="Password (6 characters minimun)"
                                                                         id="password"
                                                                         className="form__input"
+                                                                        value={password}
+                                                                        onChange={(e) => setPassword(e.target.value)}
+                                                                        onFocus={() => setPasswordFocus(true)}
+                                                                        onBlur={() => setPasswordFocus(false)}
                                                                   />
                                                             </div>
-                                                            <div className="form__label">
+                                                            <div className="form__label" onClick={toggleShowPassword}>
                                                                   <label className="eye-slash" htmlFor="password">
                                                                         <img alt="" src={icons.eyeSlash} />
                                                                   </label>
                                                             </div>
                                                       </div>
+                                                      {password !== '' && passwordFocus && !passwordValid && (
+                                                            <div className="form__error">
+                                                                  Password must be include minimun 6 characters!
+                                                            </div>
+                                                      )}
                                                 </div>
+
+                                                {/* Confirmation Password */}
                                                 <div className="form__group">
-                                                      <div className="form__row border ">
+                                                      <div
+                                                            className={
+                                                                  confirmPasswordFocus
+                                                                        ? 'form__row border active'
+                                                                        : 'form__row border '
+                                                            }
+                                                      >
                                                             <div className="form__control">
                                                                   <input
-                                                                        type="password"
+                                                                        type={
+                                                                              showConfirmationPassword
+                                                                                    ? 'text'
+                                                                                    : 'password'
+                                                                        }
                                                                         placeholder="Password comfirmation"
                                                                         id="confirm-password"
                                                                         className="form__input"
+                                                                        value={confirmPassword}
+                                                                        onChange={(e) =>
+                                                                              setConfirmPassword(e.target.value)
+                                                                        }
+                                                                        onFocus={() => setConfirmPasswordFocus(true)}
+                                                                        onBlur={() => setConfirmPasswordFocus(false)}
                                                                   />
                                                             </div>
-                                                            <div className="form__label">
+                                                            <div
+                                                                  className="form__label"
+                                                                  onClick={toggleShowconfirmationPassword}
+                                                            >
                                                                   <label
                                                                         className="eye-slash"
                                                                         htmlFor="confirm-password"
@@ -97,11 +195,21 @@ function SignUp() {
                                                                   </label>
                                                             </div>
                                                       </div>
+                                                      {confirmPassword !== '' &&
+                                                            confirmPasswordFocus &&
+                                                            !matchPassword && (
+                                                                  <div className="form__error">Password not match!</div>
+                                                            )}
                                                 </div>
 
                                                 <div className="form__group">
                                                       <div className="form__row ">
-                                                            <input id="accept-term" type="checkbox" />
+                                                            <input
+                                                                  checked={acceptTerm}
+                                                                  onChange={(e) => setAcceptTerms(e.target.checked)}
+                                                                  id="accept-term"
+                                                                  type="checkbox"
+                                                            />
                                                             <div className="form__label">
                                                                   <label className="accept-term" htmlFor="accept-term">
                                                                         I agree to the{' '}
@@ -122,9 +230,21 @@ function SignUp() {
                                                       </div>
                                                 </div>
 
+                                                {/* <div className="form__group">
+                                                      <div className="form__error">
+                                                            You must acccept terms and policy to submit form!
+                                                      </div>
+                                                </div> */}
+
                                                 <div className="form-group">
                                                       <div className="form__button">
-                                                            <button>Sign Up</button>
+                                                            <button
+                                                                  disabled={
+                                                                        !emailValid || !passwordValid || !matchPassword
+                                                                  }
+                                                            >
+                                                                  Sign Up
+                                                            </button>
                                                       </div>
                                                 </div>
                                           </form>
