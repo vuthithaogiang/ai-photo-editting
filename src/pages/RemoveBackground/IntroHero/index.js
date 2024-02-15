64672fcb882d29d83f9d22be971188012c6af121
giang <vuthithaogiang050400@gmail.com>
2024-02-15 10:08:14 +0700

    update slider tabs in remove background

--- src/pages/RemoveBackground/IntroHero/index.js
+++ src/pages/RemoveBackground/IntroHero/index.js
@@ -2,6 +2,8 @@ import classNames from 'classnames/bind';
 import styles from './IntroHero.module.scss';
 import images from '~/assets/images';
 import { useState } from 'react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 const cx = classNames.bind(styles);
 
@@ -30,6 +32,70 @@ function IntroHero() {
       ];
 
       const [tabActive, setTabActive] = useState(tabs[0]);
+      const [widthEllement, setWidthElement] = useState(0);
+      const [slideIndex, setSlideIndex] = useState(0);
+      const [start, setStart] = useState(0);
+      const [change, setChange] = useState(7);
+
+      const activeRef = useRef();
+      function dragStart(e) {
+            setStart(e.clientX);
+      }
+      function dragEnd(e) {
+            if (change > 0) {
+                  activeRef.current.scrollLeft += widthEllement;
+            } else {
+                  activeRef.current.scrollLeft -= widthEllement;
+            }
+      }
+
+      function dragOver(e) {
+            let touch = e.clientX;
+            setChange(start - touch);
+      }
+
+      useEffect(() => {
+            if (!activeRef.current) {
+                  return;
+            }
+
+            const scrollWidth = activeRef.current.scrollWidth;
+            const childrenElementCount = activeRef.current.childElementCount;
+            const width = scrollWidth / childrenElementCount;
+
+            //  console.log(activeRef.current.children[slideIndex].clientWidth);
+
+            // setWidthElement(activeRef.current.children[slideIndex].clientWidth);
+            //console.log(width);
+
+            setWidthElement(width);
+      }, [slideIndex]);
+
+      useEffect(() => {
+            if (!activeRef.current || !widthEllement) return;
+
+            // slideRef.current.unmount();
+
+            if (document.body.clientWidth < 769) {
+                  const scrollWidth = activeRef.current.scrollWidth;
+                  const childrenElementCount = activeRef.current.childElementCount;
+                  const width = scrollWidth / childrenElementCount;
+                  let numOfthumb = Math.round(activeRef.current.offsetWidth / width);
+                  console.log(numOfthumb);
+                  console.log(slideIndex);
+
+                  // if (slideIndex + 1 >= numOfthumb) {
+                  //       activeRef.current.scrollLeft = (slideIndex + 1) * widthEllement;
+                  // } else {
+                  //       activeRef.current.scrollLeft = -(slideIndex + 1) * widthEllement;
+                  // }
+                  activeRef.current.scrollLeft =
+                        slideIndex >= numOfthumb ? (slideIndex - 2) * widthEllement : -widthEllement;
+
+                  //activeRef.current.scrollLeft += widthEllement;
+            } else {
+            }
+      }, [widthEllement, slideIndex]);
 
       return (
             <div className={cx('wrapper')}>
@@ -43,20 +109,30 @@ function IntroHero() {
                               profile to your own specifications.
                         </p>
 
-                        <div className={cx('tabs')}>
-                              {tabs.map((tab) => (
-                                    <>
-                                          <div
-                                                key={tab.title}
-                                                className={
-                                                      tab.title === tabActive.title
-                                                            ? cx('nav-tab', 'active')
-                                                            : cx('nav-tab')
-                                                }
+                        <div
+                              ref={activeRef}
+                              draggable={true}
+                              onDragStart={dragStart}
+                              onDragOver={dragOver}
+                              onDragEnd={dragEnd}
+                              className={cx('tabs')}
+                        >
+                              {tabs.map((tab, index) => (
+                                    <div
+                                          key={index}
+                                          className={
+                                                tab.title === tabActive.title ? cx('nav-tab', 'active') : cx('nav-tab')
+                                          }
+                                    >
+                                          <button
+                                                onClick={() => {
+                                                      setSlideIndex(index + 1);
+                                                      setTabActive(tab);
+                                                }}
                                           >
-                                                <button onClick={() => setTabActive(tab)}>{tab.title}</button>
-                                          </div>
-                                    </>
+                                                {tab.title}
+                                          </button>
+                                    </div>
                               ))}
                         </div>
 
